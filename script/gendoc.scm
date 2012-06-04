4e3cedcff46a234b2303f722780df8b2bf426f6f
aharisu <foo.yobina@gmail.com>
2012-06-05 07:46:13 +0900

    gendoc.scmスクリプト修正

--- script/gendoc.scm
+++ script/gendoc.scm
@@ -4,25 +4,26 @@
 
 
 (define (main args)
-  (let-args (cdr args)
-    ((template "t|template=s")
-     (srcfile "s|srcfile=s")
-     (outfile "o|outfile=s")
-     (else (opt . _) (print "Unkown option : " opt))
-     . args)
-    (if (and template srcfile)
-      (let ([t-port (open-input-file template)]
-            [o-port (if outfile (open-output-file outfile) (standard-output-port))])
-        (with-ports t-port o-port (standard-error-port)
-                    (lambda ()
-                      (port-for-each print read-line)))
-        (revert-doc (geninfo srcfile) o-port)
-        (close-input-port t-port)
-        (close-output-port o-port)
-				0)
-			1)))
-
-
-
-
+  (let1 srcfile '()
+    (let-args (cdr args)
+      ((template "t|template=s")
+       (#f "s|srcfile=s"
+        => (lambda (opt) (set! srcfile (cons opt srcfile))))
+       (outfile "o|outfile=s")
+       (else (opt . _) (print "Unkown option : " opt))
+       . args)
+      (if (and template srcfile)
+        (let ([t-port (open-input-file template)]
+              [o-port (if outfile (open-output-file outfile) (standard-output-port))])
+          (with-ports t-port o-port (standard-error-port)
+                      (lambda ()
+                        (port-for-each print read-line)))
+          (for-each
+            (lambda (src)
+              (revert-doc (geninfo src) o-port))
+            srcfile)
+          (close-input-port t-port)
+          (close-output-port o-port)
+          0)
+        1))))
 
